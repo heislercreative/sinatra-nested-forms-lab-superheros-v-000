14948a1cb93b3e3125fa7e9acd6636ddba5a804d
Brennan Heisler <learn@heislercreative.com>
2018-03-18 02:19:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ class App < Sinatra::Base
   post '/teams' do
     @team = Team.new(params)
     
-    params[team][heroes]
+    params[:team][:heroes].each do |details|
+      
+    end
     
     erb :team
   end
